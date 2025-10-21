272cd54c16998a884dbda5713721c15f9a4278cf
Mango The Cat <126440708+mangothecat07@users.noreply.github.com>
2025-10-21 23:08:48 +0530

    updated

--- script.js
+++ script.js
@@ -253,21 +253,3 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 });
 
 
-
-
-
-!function(){
-  if(!window.UnicornStudio){
-      window.UnicornStudio={isInitialized:!1};
-      var i=document.createElement("script");
-      i.src="https://cdn.jsdelivr.net/gh/hiunicornstudio/unicornstudio.js@v1.4.33/dist/unicornStudio.umd.js";
-      i.onload=function(){
-          if(!window.UnicornStudio.isInitialized){
-              UnicornStudio.init();
-              window.UnicornStudio.isInitialized = !0;
-          }
-      };
-      (document.head || document.body).appendChild(i);
-  }
-}();
-
